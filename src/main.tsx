647480472fb0787a91ec48bc2b421eff44087e33
Dante Cohen <dante.cohen@tamooz.eu>
2026-07-28 17:41:04 +0300

    Add basic PWA install support

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,25 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById("root")!).render(<App />);
+import App from './App.tsx';
+
+import './index.css';
+
+createRoot(
+  document.getElementById('root')!
+).render(<App />);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener(
+    'load',
+    () => {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .catch((error) => {
+          console.error(
+            'Service worker registration failed',
+            error
+          );
+        });
+    }
+  );
+}
